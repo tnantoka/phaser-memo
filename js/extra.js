89eaac7626737c3c67b46c2dd2f0ac97d292bded
tnantoka <tnantoka@bornneet.com>
2019-05-02 00:25:13 +0900

    Deployed 5fb5307 with MkDocs version: 1.0.4

--- js/extra.js
+++ js/extra.js
@@ -1,7 +1,7 @@
 $(function() {
   $('.js-partial').each(function() {
     var $this = $(this);
-    var href = '/' + $this.data('href') + '.html?' + new Date().getTime();
+    var href = '/phaser-memo/' + $this.data('href') + '.html?' + new Date().getTime();
     $.get(href).done(function(html) {
       $this.html(html);
     });
